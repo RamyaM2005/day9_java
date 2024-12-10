8a8e4040b9ab498c6eaf32e4f4202abaaf63965f
Ramya <ramyareddym023@gmail.com>
2024-12-10 21:39:44 +0530

    Foreach_Strings.java

---
+++ Foreach_Strings.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Foreach_Strings {
+  public static void main(String[] args) {
+     Scanner sc=new Scanner(System.in);
+     String a[]={"apple","orange","mango"};
+     for(String b:a)
+     System.out.print(b+",");
+     
+  }
+}
